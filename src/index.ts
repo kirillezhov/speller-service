2ba9f1f127a87aece2d32d944c287c75d5639248
Kirill Ezhov <kirillezhov154@gmail.com>
2020-12-11 12:50:06 +0300

    index.ts: variable names to lower case

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import host from 'ip';
 import './LoggingInitialization';
 import Speller from './Speller';
 
-const PORT = 5000;
-const ADDRESS = host.address();
 const speller = new Speller();
+const port = 5000;
+const address = host.address();
 
-speller.run(PORT, ADDRESS);
+speller.run(port, address);
